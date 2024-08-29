9d8ee7b18ef4112c56af64b33611c706cc1679e0
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-08-29 16:54:21 +0700

    Change the link of backend

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/cxttonn/mock-server',
+  // baseURL: 'https://my-json-server.typicode.com/cxttonn/mock-server',
+  baseURL: 'http://localhost:8080/events?_limit=2&_page=1',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-});
+    'Content-Type': 'application/json'
+  }
+})
 
 export default {
   getEvents(perPage: number, page: number) {
-    return apiClient.get(`/events?_limit=${perPage}&_page=${page}`);
+    return apiClient.get(`/events?_limit=${perPage}&_page=${page}`)
   },
   getEvent(id: number) {
-    return apiClient.get(`/events/${id}`);
-  },
-};
+    return apiClient.get(`/events/${id}`)
+  }
+}
